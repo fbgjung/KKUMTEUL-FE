a1cc5d14e27c198c799403c1ce9b6ebc855db242
fbgjung <fbgjung@gmail.com>
2024-10-16 23:28:57 +0900

    Feat: 토글 메뉴바 클릭 시 페이지 이동 및 자녀 상태 변경 기능 추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,39 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { Container } from '../../styles/globalStyles';
-import Header from '../../components/layout/Header';
+
+
+interface RecommendBook {
+  id: number;
+  title: string;
+  image: string;
+}
+
+interface Menu {
+  id: number;
+  name: string;
+  link: string;
+  image: string;
+}
+
+interface ToggleMenu {
+  id: number;
+  name: string;
+  to: string|number;
+}
 
 const Index = () => {
   const navigate = useNavigate();
 
-  const recommendBookData = [
+  const onClickPrevButton = () => {
+    navigate(-1);
+  }
+
+  const [isToggleMenuOpen, setIsToggleMenuOpen] = useState(false);
+  const [child, setChild] = useState<number>(0); // 자녀 아이디 설정
+
+  const recommendBookData: RecommendBook[] = [
     { id: 0, title: '구름 버스 둥둥 ', image: '/assets/book1.svg' },
     { id: 1, title: '자신감 안경', image: '/assets/book2.svg' },
     { id: 2, title: '숲속에 숨어 있어', image: '/assets/book3.svg' },
@@ -14,10 +41,29 @@ const Index = () => {
     { id: 4, title: '꿈틀', image: '/assets/book1.svg' },
   ];
 
-  const menus = [
+  const menus: Menu[] = [
     { id: 0, name: 'MBTI 검사', link: '/survey', image: '/assets/survey.png' },
     { id: 1, name: '도서 목록', link: '/booklist', image: '/assets/book.png' },
   ]
+
+  const toggleMenus: ToggleMenu[] = [
+    { id: 0, name: '마이페이지', to: '/mypage'},
+    { id: 1, name: '꿈틀이1', to: 1},
+    { id: 2, name: '꿈틀이2', to: 2},
+    { id: 3, name: '꿈틀이3', to: 3},
+  ]
+
+  const toggleMenu = () => {
+    setIsToggleMenuOpen((prev) => !prev);
+  };
+
+  const onClickToggleMenuItem = (menu:ToggleMenu) => {
+    if (menu.name === '마이페이지') {
+      navigate(menu.to as string);
+    } else {
+      setChild(menu.to as number);
+    }
+  };
 
   const onClickEventBanner = () => {
     navigate('/event');
@@ -25,13 +71,26 @@ const Index = () => {
 
   return (
     <Container color="#f3f3f3">
-      <Header textcolor="#000000" color="#FFD869" nextBtnImageUrl="/assets/menu.svg" title="꿈틀" nextPage='/mypage' />
+      <Header>
+          <PrevButton onClick={onClickPrevButton} $imageurl="/assets/prev_button.svg"></PrevButton>
+          <Title>꿈틀</Title>
+          <NextButton onClick={toggleMenu} $imageurl="/assets/menu.svg"></NextButton>
+          {isToggleMenuOpen && (
+          <DropdownMenu>
+            {toggleMenus.map((toggleMenu) => (
+              <DropdownItem key={toggleMenu.id} onClick={() => onClickToggleMenuItem(toggleMenu)}>
+                <LinkTitle $color='#6EA7D0'>{toggleMenu.name}</LinkTitle>
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        )}
+      </Header>
       <Image />
       <MenuSection>
       {menus.map((menu) => (
         <Menus key={menu.id} onClick={() => navigate(menu.link)}>
           <LinkButton src={menu.image}/>
-          <LinkTitle>{menu.name}</LinkTitle>
+          <LinkTitle $color='#000000'>{menu.name}</LinkTitle>
         </Menus>
       ))}
       </MenuSection>   
@@ -58,6 +117,64 @@ const Index = () => {
 };
 
 export default Index;
+
+const Header = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #F3F3F3;
+  align-items: center; 
+  position: sticky;
+  top: 0; 
+  left: 0;
+  z-index: 1000;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DropdownMenu = styled.div`
+  position: absolute;
+  top: 60px;
+  right: 10px;
+  width: 120px;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  z-index: 1001;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+`;
+
+const DropdownItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  cursor: pointer;
+  width: 100%;
+  justify-content: center;
+`;
+
+const Title = styled.h2`
+  margin: 0;
+  color: #000000;
+  font-size: 18px;
+`;
+
+const PrevButton = styled.button<{$imageurl: string}>`
+  width: 30px;
+  height: 30px;
+  padding:0;
+  background: no-repeat center/cover url(${({ $imageurl }) => $imageurl});
+`
+const NextButton = styled.button<{$imageurl: string}>`
+  width: 25px;
+  height: 25px;
+  padding: 0;
+  background: no-repeat center/cover url(${({ $imageurl }) => $imageurl});
+  overflow: hidden;
+`
 
 const Image = styled.div`
   width: 600px;
@@ -95,10 +212,14 @@ const LinkButton = styled.img`
   margin-bottom: 8px;
 `
 
-const LinkTitle = styled.p`
+const LinkTitle = styled.p<{$color: string}>`
   font-size: 12px;
   padding: 0;
   margin: 0;
+  &:hover {
+    color: ${({ $color }) => $color}
+  }
+  
 `
 
 // 이벤트 배너
@@ -127,7 +248,7 @@ const RecommendBookSection = styled.div`
   width: calc(100% - 40px);
   background-color: #ffffff;
   border-radius: 20px;
-  margin: 0 10px 90px 10px;
+  margin: 12px 10px 90px 10px;
   padding: 20px 20px 40px 20px;
   text-align: left;
   display: flex;
