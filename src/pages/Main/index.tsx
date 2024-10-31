57916b17c175431879406eba7da25da72f3b5671
fbgjung <fbgjung@gmail.com>
2024-10-31 11:49:49 +0900

    Chore: 불필요한 코드 제거

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -122,8 +122,6 @@ const Index = () => {
           }
       };
 
-      setIsLoggedIn(false);
-
       fetchChildProfiles();
       fetchChildProfileAndRecommendedBooks();  
       fetchCurrentEvent();
